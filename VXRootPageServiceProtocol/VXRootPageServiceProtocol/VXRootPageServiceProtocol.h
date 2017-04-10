e61e4cbf50192384c0fc9694bd877e75ac1d3e27
voidxin <zhangxin409999@163.com>
2017-04-10 17:16:15 +0800

    first commit

---
+++ VXRootPageServiceProtocol/VXRootPageServiceProtocol/VXRootPageServiceProtocol.h
@@ -0,0 +1,15 @@
+//
+//  VXRootPageServiceProtocol.h
+//  VXRootPageServiceProtocol
+//
+//  Created by voidxin on 17/4/10.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol VXRootPageServiceProtocol <NSObject>
+
+- (UIViewController *)rootPageViewControllerWith:(id)Object;
+
+@end
